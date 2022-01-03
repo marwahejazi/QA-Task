e56918ff4bea537495c686f844f14ef0fb1dc57c
marwahejazi <56307453+marwahejazi@users.noreply.github.com>
2022-01-03 14:18:51 +0200

    updated

--- hotels/cypress/integration/search.ts
+++ hotels/cypress/integration/search.ts
@@ -1,35 +1,34 @@
 /// <reference types="cypress" />
-
-import HomePageActions from "./pageObject/homePage/actions"
-import HotelPageAssertions from "./pageObject/hotelPage/assertions"
+import HomePageActions from "../pageObject/homePage/actions"
+import HotelPageAssertions from "../pageObject/hotelPage/assertions"
 
 const hotelPageAssertion = new HotelPageAssertions
-
-
 const homePageActions = new HomePageActions
 const location = "Jerusalem"
-const location2 = "Nablus"
-const location3 = "Nables"
-it.only('Hotels test', function () {
+const location2 = "Nables"
+const location3 = "Nablus"
+
+it.only('Hotels test Jerusalem', function () {    //Positive testing
     homePageActions.openHomePage()
         .typeInSearchInputField(location)
         .clickOnSearchButton()
         .openSpecificResult()
-
     hotelPageAssertion.checkHotelLocation(location)
 
+})
+it.only('Hotels test Nables', function () {    //Negative testing
     homePageActions.openHomePage()
         .typeInSearchInputField(location2)
         .clickOnSearchButton()
         .openSpecificResult()
-
     hotelPageAssertion.checkHotelLocation(location2)
 
+})
+it.only('Hotels test Nablus', function () {      //Positive testing
     homePageActions.openHomePage()
         .typeInSearchInputField(location3)
         .clickOnSearchButton()
         .openSpecificResult()
-
     hotelPageAssertion.checkHotelLocation(location3)
 
 })
